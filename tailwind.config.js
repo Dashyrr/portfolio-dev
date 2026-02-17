96157b02172f0eddb358397f4d4253a3858d2030
Dashyrr <113636607+Dashyrr@users.noreply.github.com>
2026-02-17 17:09:11 +0100

    Setup du projet

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                50: "rgb(17 17 17)",
+                100: "rgb(34 34 34)",
+                500:"rgb(72 72 72)",
+                800:"rgb(238 238 238)"
+                }
+            }
+        }
+    }
+}
