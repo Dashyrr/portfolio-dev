34fdc2509cd636e4be1a5a0acda152514dc948a6
Dashyrr <mylan.cordier@gmail.com>
2026-02-20 14:50:08 +0100

    Skills et projets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,24 @@
 tailwind.config = {
-    theme: {
-        extend: {
-            colors: {
-                brand: {
-                50: "rgb(17 17 17)",
-                100: "rgb(34 34 34)",
-                500:"rgb(72 72 72)",
-                800:"rgb(238 238 238)"
+            theme: {
+                extend: {
+                    colors: {
+                        brand: {
+                            50: "rgb(17,17,17)",
+                            100: "rgb(34,34,34)",
+                            500: "rgb(72,72,72)",
+                            800: "rgb(238,238,238)"
+                        },
+                        accent: "#faed27",
+                        aHov : "#A2991A"
+                    },
+                    fontFamily: {
+                        serif: ['"DM Serif Display"', 'serif'],
+                        mono:  ['"DM Mono"', 'monospace'],
+                    },
+                    fontSize:{
+                        big: "1.4rem",
+                        small: "0.8rem"
+                    }
                 }
             }
         }
-    }
-}
